821bde2ad070981f28ef76b40e8ad3bdde359eeb
21harley <john.llanes.1996@gmail.com>
2025-10-01 07:51:02 -0400

    add fix-page-crud-user

--- app/usuarios/page.tsx
+++ app/usuarios/page.tsx
@@ -1,12 +1,25 @@
 'use client'
 import CrudUser from "@/components/crudUser/crudUser"
+import useUserStore from "../store/store";
+import LoadingCar from "@/components/loadingCar/loadingCar";
 
 export default function Usuarios(){
-  return (
-    <>
-    <section className="_color_four h-auto min-h-[80dvh] grid place-items-center">
-        <CrudUser/>
-    </section>
-    </>
-  )
+  const {user,isLogout} = useUserStore();
+
+  if ( isLogout) {
+      return <LoadingCar redirect={isLogout}></LoadingCar>;
+  }
+
+  if(user?.id){
+    return (
+      <>
+      <section className="_color_four h-auto min-h-[80dvh] grid place-items-center">
+          <CrudUser/>
+      </section>
+      </>
+    )
+  }else{
+    return <LoadingCar redirect={true}></LoadingCar>;
+  }
+ 
 }
